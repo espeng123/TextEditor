225cba9e3d7f3e6b7b0c5c2e66b48e961951ab90
dlin <dlin@WINFA18-RPTPGM2.smes.org>
2019-09-19 23:18:48 -0700

    branch test

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Is this working?");
+		System.out.println("Branch test - should not appear in master");
 
 	}
 
