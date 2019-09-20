2e407c885f3c2c50cc2a5346c35be137c3ed59a2
dlinsmes <david.lin@smes.org>
2019-09-19 23:01:07 -0700

    branch test

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Is this working?");
-
+		
+		System.out.println("This should be a branch (separate from master)");
+		System.out.println("Shouldn't be committed until everyone reviews");
 	}
 
 }
